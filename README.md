58345eae12a77442a704c968c0659341778e94a6
Rachel Yeung <rachel.cmyeung@gmail.com>
2020-02-04 20:43:44 -0800

    updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# fitnessTracker
+# Fitness Tracker
+
+## Description
+This application is a workout/fitness tracker that allows a user to create, track, and view daily workouts. They are given the option to log multiple workouts a day, and the application tracks the name, type, weight, sets, reps, and duration if it is a resistance exercise, and tracks the distance travelled if it is cardio. All of this information is populated in different charts for easy readability and as a way for users to look at all their workouts as a whole. Being able to track and keep progress of workouts will allow users to reach their fitness goals quicker.
+
+## Demo
+
+![gif](images/demo.gif)
+
+## Technologies Used
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML): used for structuring and creating elements on the DOM
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS): used to style html elements on the page
+* [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript): high level programming language
+* [Node.js](https://developer.mozilla.org/en-US/docs/Web/API/Node): JavaScript runtime, allows users to run JavaScript on the server
+* [Express](https://expressjs.com/): Web framwork for node.js
+* [Morgan](https://www.npmjs.com/package/morgan/v/1.1.1): Logging middleware for node.js http apps
+* [MongoDB](https://www.mongodb.com/): document database
+* [Mongoose](https://mongoosejs.com/): mongodb object modeling for node.js
+
+## Code Snippet
+
+```
+ app.put("/api/workouts/:id", (req, res) => {
+
+        db.Exercise.create(req.body)
+            .then((data) => db.Workout.findOneAndUpdate(
+                {_id: req.params.id},
+                { 
+                    $push: {
+                        exercises: data._id 
+                    }, 
+                    $inc: {
+                        totalDuration: data.duration
+                    } 
+                },
+                { new: true })
+            )
+            .then(dbWorkout => {
+            res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+      });
+
+
+```
+This portion of the script file displays the PUT route for adding an exercise to a workout. First, '.create' is called on the Exercise model to save a document (req.body) to the database. This is then updated in the Workout model based on the unique id. THe '$push' operator appends the exercise ID to the array. The '$inc' object ensures that the total duration of a workout is updated if an exercise is added. The 'new' option is also set to true to return the document after the update was run. This information is finally returned as a JSON response. 
+
+## Authors
+
+**Rachel Yeung**
+* [Portfolio](https://xrachhel.github.io/updatedPortfolio/)
+* [Github](https://github.com/xrachhel)
+* [LinkedIn](https://www.linkedin.com/in/rachel-yeung-814986159/)
+
--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,18 +1,6 @@
 const db = require("../models")
-const mongojs = require("mongojs")
 
 module.exports = function (app) {
-    // app.post("/api/workouts", ({body}, res) => {
-    //     db.Exercise.create(body)
-    //     .then(({_id}) => db.Workout.findOneAndUpdate({}, {$push:{exercise:_id}}, {new:true}))
-    //     .then(dbWorkout => {
-    //         res.json(dbWorkout)
-    //     })
-    //     .catch(err => {
-    //         res.json(err)
-    //     });
-    // });
-
 
     // GET routes
     app.get("/api/workouts", (req, res) => {
@@ -26,16 +14,6 @@ module.exports = function (app) {
           });
     });
 
-    // app.get("/api/workouts/:id", (req, res) => {
-    //     db.Workout.find({ _id: mongojs.ObjectId(req.params.id) })
-    //         .then(dbWorkout => {
-    //             res.json(dbWorkout);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
-    // })
-
     app.get("/api/workouts/range", (req, res) => {
         db.Workout.find({})
             .populate("exercises")
@@ -47,35 +25,7 @@ module.exports = function (app) {
             });
     })
 
-    // app.get("/api/exercises", (req, res) => {
-    //     db.Exercise.find({})
-    //         .then(dbExercise => {
-    //             res.json(dbExercise);
-    //         })
-    //         .catch(err => {
-    //             res.json(err);
-    //         });
-    // });
-    // POST routes
-    // app.post("/api/workouts", ({ body }, res) => {
-    //     db.Exercise.create(body)
-    //         .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercise: _id } }, { new: true }))
-    //         .then(dbWorkout => {
-    //             res.json(dbWorkout)
-    //         })
-    //         .catch(err => {
-    //             res.json(err)
-    //         });
-    // });
-    // app.post("/api/workouts", (req, res) => {
-    //     db.Workout.create({})
-    //     .then(dbWorkout => {
-    //         res.json(dbWorkout)
-    //     })
-    //     .catch(err => {
-    //         res.json(err)
-    //     })
-    // })
+    // POST route
     app.post("/api/workouts",(req,res)=>{
         db.Workout.create({day: Date.now()})
             .then(workout=>{
@@ -86,9 +36,8 @@ module.exports = function (app) {
     });
 
 
-    // PUT routes
+    // PUT route
     app.put("/api/workouts/:id", (req, res) => {
-        console.log(req.body);
 
         db.Exercise.create(req.body)
             .then((data) => db.Workout.findOneAndUpdate(
@@ -110,52 +59,5 @@ module.exports = function (app) {
             });
       });
 
-    // app.put("/api/workouts/:id", (req, res) => {
-    //     console.log(req.body)
-    //     db.Exercise.create(req.body)
-    //     .then((data) => db.Workout.findOneAndUpdate({_id:req.params.body}, 
-    //     {
-    //         $push : {
-    //             exercises: data._id
-    //         }, 
-    //     $inc : {totalDuration:data.duration}}, 
-    //         {new: true}))
-    //     .then(dbWorkout => {
-    //         res.json(dbWorkout)
-    //     })
-    //     .catch(err => {
-    //         res.json(err)
-    //     })
-    // })
-    // app.put("/api/workouts/:id", (req, res) => {
-    //     db.Exercise.updateOne({ _id: req.params.id },
-    //         {
-    //             $set:
-    //             {
-    //                 "name": req.body.name,
-    //                 "weight": req.body.weight,
-    //                 "sets": req.body.sets,
-    //                 "reps": req.body.reps,
-    //                 "duration": req.body.duration,
-    //                 "distance": req.body.distance
-    //             }
-    //         })
-    //         .then(dbWorkout => {
-    //             res.json(dbWorkout)
-    //         })
-    //         .catch(err => {
-    //             res.json(err)
-    //         })
-    // })
-    // app.put("/api/workouts/:id", function(req,res){
-    //     db.Exercise.create(req.body, function(err, docs){
-    //         if(err) throw err;
-    //         db.Workout.update(
-    //             {
-    //                 _id: mongojs.ObjectId(req.params.id)
-    //             },{$push:{exercises: docs._id}}, {new:true}).then(function(data){
 
-    //         })
-    //     });
-    // })
 }
